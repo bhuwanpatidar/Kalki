338c042a52e5de6c2735f11a263aa82ddf5434a0
Bhuwan Patidar <67598622+bhuwanpatidar@users.noreply.github.com>
2023-06-13 20:27:18 +0530

    Add files via upload

---
+++ TO/Need.sh
@@ -0,0 +1,14 @@
+sudo apt install amass
+sudo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest && sudo cp /root/go/bin/subfinder /usr/bin
+sudo go install -v https://github.com/tomnomnom/anew@latest && sudo cp /root/go/bin/anew /usr/bin
+sudo apt install httprobe 
+sudo apt install assetfinder
+sudo git clone https://github.com/obheda12/GitDorker.git ./Tools/gitdorker
+pip install -r ./Some.txt
+sudo apt install xterm
+sudo go install -v github.com/lc/subjs@latest && sudo cp /root/go/bin/subjs /usr/bin
+sudo apt install nuclei 
+sudo apt install sed
+sudo apt install jq
+sudo git clone https://github.com/christophetd/censys-subdomain-finder.git  ./Tools/censys
+sudo apt install nikto
