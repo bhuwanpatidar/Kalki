d80ada85434d2da9689e104d2c61476fab3ae1af
Kalki <67598622+bhuwanpatidar@users.noreply.github.com>
2024-01-08 16:00:46 +0530

    Update install.sh

--- Recon/Tool/install.sh
+++ Recon/Tool/install.sh
@@ -1,18 +1,10 @@
 sudo apt install golang-go
-git clone https://github.com/guelfoweb/knock.git
-git clone https://github.com/danielmiessler/SecLists.git
-git clone https://github.com/TheRook/subbrute.git
-git clone https://github.com/robre/scripthunter.git
-git clone https://github.com/aboul3la/Sublist3r.git
-git clone https://github.com/tomnomnom/assetfinder.git
-git clone https://github.com/jobertabma/relative-url-extractor.git
 sudo  go install -v github.com/tomnomnom/assetfinder@latest && sudo cp /root/go/bin/assetfinder /usr/bin/
-sudo cp /root/go/bin/assetfinder /usr/local/go/bin/
 sudo snap install httpx
 sudo apt install npm
 sudo apt install snap
 sudo apt install arjun
-sudo apt install xterm
+git clone https://github.com/danielmiessler/SecLists.git
 npm -g install js-beautify
 sudo apt install amass
 sudo apt-get install python-dnspython
@@ -20,7 +12,6 @@ sudo yum install python-argparse
 sudo apt-get install python-requests
 sudo go install -v github.com/tomnomnom/anew@latest && sudo cp /root/go/bin/anew /usr/bin/
 sudo go install -v github.com/tomnomnom/httprobe@latest &&sudo  cp /root/go/bin/httprobe /usr/bin/
-sudo go install -v github.com/sensepost/gowitness@latest  && sudo  cp /root/go/bin/gowitness /usr/bin/
 sudo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest && sudo cp /root/go/bin/subfinder /usr/bin/
 sudo go install -v github.com/tomnomnom/waybackurls@latest && sudo   cp /root/go/bin/waybackurls /usr/bin/
 sudo go install -v github.com/lc/gau/v2/cmd/gau@latest && sudo  cp /root/go/bin/gau /usr/bin/
